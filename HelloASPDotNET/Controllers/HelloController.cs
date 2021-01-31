ad86f49a157947ad10ce7ff29313234d143ac516
Kelly Thomas <kellydye77@gmail.com>
2021-01-31 15:19:12 -0600

    Commit all

--- HelloASPDotNET/Controllers/HelloController.cs
+++ HelloASPDotNET/Controllers/HelloController.cs
@@ -21,7 +21,8 @@ namespace HelloASPDotNET.Controllers
         [Route("/hello")]
         public IActionResult Welcome(string name = "World")
         {
-            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+            ViewBag.person = name;
+            return View();
         }
     }
 }
